a9f5b282900bfeee33736047b8bbc696821d5e70
slqi <76856549+slqi@users.noreply.github.com>
2022-02-17 09:36:24 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # slqi.github.io
-site
---
+++ assets/javascript/app.js
@@ -0,0 +1,64 @@
+// Made by Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
+// Credits to expl0it, shellcode.team
+// GitHub: https://github.com/Lumm1t/obnoxious.club
+
+var _app = function () {
+    this.id = 0;
+    this.videoElement = null;
+    this.audioElement = null;
+    this.musicVolume = 0.12;
+    this.musicFadeIn = 4000;
+    this.skippedIntro = false;
+    this.backgroundToggler = false;
+    this.shouldIgnoreVideo = false;
+    this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
+    this.brandDescription = [
+        "toxic khayos", 
+        "retards", 
+        "black killers", 
+        "allah fanboys",
+        "greasy pussy slayers", 
+        "bashar fandom", 
+        "we love karam family"
+    ];
+
+    this.titleChanger = function (text, delay) {
+        text = text ||
+            ['chino', 'elaina', 'salma', 'rip nick', 'destiny is black', 'kill da blacks', '=', 'camelsquad', 'fav song is dynamic duo v2'];
+        delay = delay || 500;
+
+        var counter = 0;
+
+        setInterval(function () {
+            if(counter < text.length) 
+                document.title = text[counter++]; 
+            else 
+                document.title = text[counter = 0];
+        }, delay);
+    }
+
+    this.iconChanger = function (urls, delay) {
+        if (!urls)
+            return;
+
+        delay = delay || 500;
+
+        var counter = 0;
+        
+        setInterval(function () {
+            if(counter < urls.length) {
+                var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+                link.type = 'image/x-icon';
+                link.rel = 'shortcut icon';
+                link.href = urls[counter];
+                document.getElementsByTagName('head')[0].appendChild(link);
+            }
+            else 
+                counter = 0;
+
+           ++counter;
+        }, delay);
+    }
+};
+
+var app = new _app();
